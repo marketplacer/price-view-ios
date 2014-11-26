70bd22260dbd08da6dda1607593a6564b80d4259
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-26 17:01:02 +1100

    Show price view

--- price-view/TegPriceView.swift
+++ price-view/TegPriceView.swift
@@ -9,11 +9,70 @@
 import UIKit
 
 class TegPriceView: UIView {
+  var size = CGSize()
+
+  var priceFont = UIFont.boldSystemFontOfSize(19)
+  var beforeDiscountFont = UIFont.systemFontOfSize(19)
+
+  var marginBetweenPrices: CGFloat = 5
+
   override init(frame: CGRect) {
     super.init(frame: frame)
   }
 
   required init(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
+  }
+
+  func show(price: String, priceBeforeDiscount: String? = nil) {
+    layer.sublayers = nil
+
+    let priceLayer = TegPriceView.createTextLayer(price, font: priceFont)
+    layer.addSublayer(priceLayer)
+
+    size = priceLayer.bounds.size
+
+    if let currentPriceBeforeDiscount = priceBeforeDiscount {
+      let priceBeforeDiscountLayer = TegPriceView.addPriceBeforeDiscount(
+        currentPriceBeforeDiscount,
+        font: beforeDiscountFont, superlayer: layer,
+        position: CGPoint(x: priceLayer.bounds.maxX + marginBetweenPrices, y: 0))
+
+      size.width += priceBeforeDiscountLayer.bounds.width + marginBetweenPrices
+    }
+
+    invalidateIntrinsicContentSize()
+  }
+
+  private class func addPriceBeforeDiscount(text: String, font: UIFont,
+    superlayer: CALayer, position: CGPoint) -> CATextLayer {
+
+    let priceBeforeDiscountLayer = TegPriceView.createTextLayer(text, font: font)
+
+    superlayer.addSublayer(priceBeforeDiscountLayer)
+
+    priceBeforeDiscountLayer.position = position
+
+    return priceBeforeDiscountLayer
+  }
+
+
+  private class func createTextLayer(text: String, font: UIFont) -> CATextLayer {
+    let size = NSString(string: text).sizeWithAttributes([NSFontAttributeName: font])
+
+    let layer = CATextLayer()
+    layer.font = CGFontCreateWithFontName(font.fontName)
+    layer.fontSize = font.pointSize
+    layer.foregroundColor = UIColor.blackColor().CGColor
+    layer.string = text
+    layer.bounds = CGRect(origin: CGPoint(), size: size)
+    layer.contentsScale = UIScreen.mainScreen().scale
+    layer.anchorPoint = CGPoint()
+
+    return layer
+  }
+
+  override func intrinsicContentSize() -> CGSize {
+    return size
   }
 }
--- price-view/ViewController.swift
+++ price-view/ViewController.swift
@@ -10,15 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+  @IBOutlet weak var priceView: TegPriceView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
-    showPrices()
-  }
-
-  private func showPrices() {
+    priceView.backgroundColor = nil
   }
 
 
+  @IBAction func onShowOnePriceTapped(sender: AnyObject) {
+    priceView.show("$12.99")
+  }
+
+  @IBAction func onShowTwoPricesTapped(sender: AnyObject) {
+    priceView.show("$1,200", priceBeforeDiscount: "$1.300")
+  }
 }
 
