8e0cbc37b1e42c1eb0ab476e530412dbd7e66846
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-27 10:12:41 +1100

    Customise cross out line color

--- README.md
+++ README.md
@@ -1,17 +1,26 @@
 # iOS control for showing price.
 
-This control shows two prices next to each other: regular price and price before discount.
+This control shows tow prices next to each other: regular price and price before discount.
 The latter price is crossed out.
 
 <img src='https://dl.dropboxusercontent.com/u/11143285/bikeexchange/github_images/price-view-ios-screenshot.png' width='160' alt='Price control for iOS'>
 
- 
+
 ## Usage
 
 ```
 let priceView = TegPriceView()
 priceView.showPrice('$199')
 priceView.showPrice('$199', '$299') # second price is the price before discount
+
+# Customize fonts (optional)
+priceView.priceFont = UIFont.boldSystemFontOfSize(15)
+priceView.priceBeforeDiscountFont = UIFont.systemFontOfSize(15)
+
+# Customize colors (optional)
+priceView.priceColor = UIColor.blackColor()
+priceView.priceBeforeDiscountColor = TegColors.grayColor()
+
 ```
 
 One can also use TegPriceView with the storyboard.
--- price-view/TegPriceView.swift
+++ price-view/TegPriceView.swift
@@ -10,32 +10,33 @@ import UIKit
 
 class TegPriceView: UIView {
   var size = CGSize()
-
+  
   var priceFont = UIFont.boldSystemFontOfSize(19)
-  var beforeDiscountFont = UIFont.systemFontOfSize(19)
+  var priceBeforeDiscountFont = UIFont.systemFontOfSize(19)
   
   var priceColor = UIColor.blackColor()
   var priceBeforeDiscountColor = UIColor.grayColor()
-
+  
   var marginBetweenPrices: CGFloat = 5
-
+  
   func show(price: String, priceBeforeDiscount: String? = nil) {
     layer.sublayers = nil
-
+    
     // Show price
     let priceLayer = CATextLayer()
     TegPriceView.styleTextLayer(priceLayer, text: price, font: priceFont)
     priceLayer.foregroundColor = priceColor.CGColor
     layer.addSublayer(priceLayer)
-
+    
     size = priceLayer.bounds.size
- 
+    
     // Show price before discount
     if let currentPriceBeforeDiscount = priceBeforeDiscount {
       let priceBeforeDiscountLayer = TegStrikethroughTextLayer()
+      priceBeforeDiscountLayer.color = priceBeforeDiscountColor
       
       TegPriceView.styleTextLayer(priceBeforeDiscountLayer,
-        text: currentPriceBeforeDiscount, font: beforeDiscountFont)
+        text: currentPriceBeforeDiscount, font: priceBeforeDiscountFont)
       
       layer.addSublayer(priceBeforeDiscountLayer)
       
@@ -46,20 +47,20 @@ class TegPriceView: UIView {
       
       size.width += priceBeforeDiscountLayer.bounds.width + marginBetweenPrices
     }
-
+    
     invalidateIntrinsicContentSize()
   }
-
+  
   private class func styleTextLayer(layer: CATextLayer, text: String, font: UIFont) -> CATextLayer {
     let size = NSString(string: text).sizeWithAttributes([NSFontAttributeName: font])
-
+    
     layer.font = CGFontCreateWithFontName(font.fontName)
     layer.fontSize = font.pointSize
     layer.string = text
     layer.bounds = CGRect(origin: CGPoint(), size: size)
     layer.contentsScale = UIScreen.mainScreen().scale
     layer.anchorPoint = CGPoint()
-
+    
     return layer
   }
   
