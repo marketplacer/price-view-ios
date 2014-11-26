cac5358a60de04427110c3b99e46c30c7db47995
Evgenii Neumerzhitckii <evgeny@bikeexchange.com.au>
2014-11-26 16:08:28 +1100

    Price view

---
+++ price-view/TegPriceView.swift
@@ -0,0 +1,19 @@
+//
+//  PriceView.swift
+//  price-view
+//
+//  Created by Evgenii Neumerzhitckii on 26/11/2014.
+//  Copyright (c) 2014 The Exchange Group Pty Ltd. All rights reserved.
+//
+
+import UIKit
+
+class TegPriceView: UIView {
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+  }
+
+  required init(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+  }
+}
--- price-view/ViewController.swift
+++ price-view/ViewController.swift
@@ -12,12 +12,11 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view, typically from a nib.
+
+    showPrices()
   }
 
-  override func didReceiveMemoryWarning() {
-    super.didReceiveMemoryWarning()
-    // Dispose of any resources that can be recreated.
+  private func showPrices() {
   }
 
 
